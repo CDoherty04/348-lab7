893ee09df1665a9d3240d26ca8b06d3491734942
Charlie <charlieedoherty@gmail.com>
2024-11-03 20:12:59 -0600

    Implement readStdio and display

--- matrix-template-1.cpp
+++ matrix-template-1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -10,46 +11,65 @@ private:
 
 public:
     // 1. Read values from stdin into a matrix
-    void readFromStdin();
+    void readFromStdin() {
+        
+        char filename[] = "matrix-data.txt";
+        ifstream file(filename);
+
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                file >> data[i][j];
+            }
+        }
+        file.close();
+    };
+
     // 2. Display a matrix
-    void display() const;
+    void display() const {
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                cout << data[i][j] << " ";
+            }
+            cout << endl;
+        }
+    };
+    
     // 3. Add two matrices (operator overloading for +)
-    Matrix operator+(const Matrix& other) const;
+    // Matrix operator+(const Matrix& other) const;
     // 4. Multiply two matrices (operator overloading for *)
-    Matrix operator*(const Matrix& other) const;
+    // Matrix operator*(const Matrix& other) const;
     // 5. Compute the sum of matrix diagonal elements
-    int sumOfDiagonals() const;
+    // int sumOfDiagonals() const;
     // 6. Swap matrix rows
-    void swapRows(int row1, int row2);
+    // void swapRows(int row1, int row2);
 };
 
 int main() {
     // Example usage:
     Matrix mat1;
-    cout << "Enter values for Matrix 1:" << endl;
     mat1.readFromStdin();
     cout << "Matrix 1:" << endl;
     mat1.display();
 
-    Matrix mat2;
-    cout << "Enter values for Matrix 2:" << endl;
-    mat2.readFromStdin();
-    cout << "Matrix 2:" << endl;
-    mat2.display();
+    // Matrix mat2;
+    // cout << "Enter values for Matrix 2:" << endl;
+    // mat2.readFromStdin();
+    // cout << "Matrix 2:" << endl;
+    // mat2.display();
 
-    Matrix sum = mat1 + mat2;
-    cout << "Sum of matrices:" << endl;
-    sum.display();
+    // Matrix sum = mat1 + mat2;
+    // cout << "Sum of matrices:" << endl;
+    // sum.display();
 
-    Matrix product = mat1 * mat2;
-    cout << "Product of matrices:" << endl;
-    product.display();
+    // Matrix product = mat1 * mat2;
+    // cout << "Product of matrices:" << endl;
+    // product.display();
     
-    cout << "Sum of diagonals of Matrix 1: " << mat1.sumOfDiagonals() << endl;
+    // cout << "Sum of diagonals of Matrix 1: " << mat1.sumOfDiagonals() << endl;
 
-    mat1.swapRows(0, 2);
-    cout << "Matrix 1 after swapping rows:" << endl;
-    mat1.display();
-    
+    // mat1.swapRows(0, 2);
+    // cout << "Matrix 1 after swapping rows:" << endl;
+    // mat1.display();
+
     return 0;
 }
